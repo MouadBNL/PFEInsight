2f544065f38bbdf0a10f0d67eac8c3507f080cc6
MouadBNL <mouadbnl.2k01@gmail.com>
2021-12-17 14:49:44 +0100

    installed tailwindcss

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { router } from './router'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import './assets/tw.css'
 
 const app = createApp(App)
 app.use(createPinia())
