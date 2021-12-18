00c9be500ab64fb01ec5b70ec3e6e417e2c9ac11
MouadBNL <mouadbnl.2k01@gmail.com>
2021-12-18 13:13:01 +0100

    adjustement to the ui

--- client/src/services/useAxios.ts
+++ client/src/services/useAxios.ts
@@ -1,50 +1,80 @@
 import { ref } from 'vue'
-import axios, { AxiosResponse } from "axios"
+import axios, { AxiosResponse, AxiosRequestConfig , Method, AxiosError } from "axios"
+import { useMessage } from 'naive-ui';
 
 const apiClinet = axios.create({
     baseURL: 'http://localhost:8000/api/v1'
 })
 
-export const useAxios = <T = any>(url: string, config: Object= {}) => {
+interface GetExceptionInterface {type: 'error' | 'info' | 'success' | 'warning', message: string}
+type HandleErrorInterface = (err: any) => GetExceptionInterface|null|undefined;
 
+export const useAxios = <T = any>(config: Object = {}, handleException?: HandleErrorInterface) => {
 
-
-    const response = ref<AxiosResponse<T>>()
-    const data = ref<{status: string, message:string, data:T}>()
-    const error = ref<any>()
+    const m         = useMessage()
+    const response  = ref<AxiosResponse<{status: 'success' | 'invalid' | 'error', message: string, data?: T}>|null>()
+    const data      = ref<T|null>()
     const isLoading = ref<boolean>(false)
 
+    const getException = (err: any): GetExceptionInterface => {
+        if(handleException) {
+            const ex = handleException(err)
+            if(ex) return ex
+        }
+        let message = "Une erreur est survenue, veuillez réessayer plus tard."
+        if(err?.data?.errors) {
+            const errors = err.data.errors
+            if(typeof errors == 'string') message = errors
+            else if(typeof errors == 'object') {
+                const firstval = errors[Object.keys(errors)[0]]
+                if(typeof firstval == 'string') message = firstval
+                else if(Array.isArray(firstval) && firstval.length > 0) message = firstval[0]
+                else message = JSON.stringify(errors)
+            }
+        }
+        else if(err?.data?.message && typeof err.data.message == 'string') message = err.date.message
+        else if(err?.message && typeof err.message == 'string') message = err.message
+
+        return {type: 'error', message: message}
+    }
+
+    const showMessage = (ex: GetExceptionInterface) => {
+        if(ex.type == 'success') m.success(ex.message)
+        else m.error(ex.message)
+    }
+
     const fetch = async (payload = {}) => {
+        response.value = null
+        data.value = null
         isLoading.value = true
         try {
             const res = await apiClinet.request({
-                url,
                 data: {
                     ...payload
                 },
                 ...config,
-            })
-            if(res.data.status != 'success'){
-                throw res.data.message
-            }
+            }).catch(err => {throw err})
+            if(res.data.status != 'success')
+                throw res
+            
             response.value = res
             data.value = res.data
         } catch (err: any) {
-            error.value = err
-            throw err
+            response.value = null
+            showMessage( getException(err.response) )
         } finally {
             isLoading.value = false
         }
     }
 
-    return { response, data, error, isLoading, fetch }
+    return { response, data, isLoading, fetch }
 }
 
-export const setAuthorizationHeader = (token: string | null) => {
-    apiClinet.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    if(token) {
-        localStorage.setItem('jwt_token', token)
-    } else {
-        localStorage.removeItem('jwt_token')
-    }
-}
+// export const setAuthorizationHeader = (token: string | null) => {
+//     apiClinet.defaults.headers.common['Authorization'] = `Bearer ${token}`
+//     if(token) {
+//         localStorage.setItem('jwt_token', token)
+//     } else {
+//         localStorage.removeItem('jwt_token')
+//     }
+// }
--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="h-screen w-screen flex items-center justify-center bg-gray-100">
-        <NCard class="w-5/12">
-            <h1 class="text-center text-3xl font-extrabold mb-8">PFE Insight</h1>
+    <div class="h-screen max:w-screen flex items-center justify-center bg-gray-100 p-4">
+        <NCard class="w-11/12 md:w-5/12 xl:w-3/12">
+            <h1 class="text-center text-3xl font-extrabold mb-8 mt-0">PFE Insight</h1>
             <NForm
                 :model="credentials"
+                :rules="rules"
                 ref="formRef"
-    
+                @submit.prevent="handleLogin"
             >
-                <NFormItem label="Email" path="credentials.email">
+                <NFormItem label="Email" path="email">
                     <NInput v-model:value="credentials.email" placeholder="adc@xyz.com" autofocus />
                 </NFormItem>
-                <NFormItem label="Mod de passe" path="credentials.password">
-                    <NInput v-model:value="credentials.password" placeholder="******"/>
+                <NFormItem label="Mod de passe" path="password">
+                    <NInput v-model:value="credentials.password" placeholder="******" type="password"/>
                 </NFormItem>
                 <NFormItem>
                     <NButton @click="handleLogin" type="primary" :loading="login.isLoading.value">Connexion</NButton>
@@ -26,14 +27,46 @@ import { reactive, ref } from 'vue'
 import { NCard ,NForm, NFormItem, NInput, NButton } from 'naive-ui'
 import { useLogin } from '@/services/useAuth'
 
-const formRef = ref(null)
-const credentials = reactive({
+const formRef = ref<any>(null)
+const credentials = ref({
     email: '',
     password: ''
 })
+const rules = {
+    email: [{
+        required: true,
+        validator(rule:any, value:any) {
+            if(!value) {
+                return new Error('un e-mail est requis')
+            }
+            else if(! /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) return new Error('veuillez entrer un email valide')
+            return true
+        },
+        trigger: ['input'],
+    }],
+    password: [{
+        required: true,
+        // validator (rule:any, value:any) {
+        //     if(!value) throw new Error('un mot de passe est requis')
+        //     return true
+        // },
+        trigger: ['input', 'blur'],
+        message: 'un mot de passe est requis'
+    }]
+}
 const login = useLogin();
 
-const handleLogin = async () => {
-     await login.login(credentials)
+const handleLogin = async (e:any) => {
+    let passVlidation = false
+    await formRef.value?.validate(async (errors: Array<any>|null|undefined) => {
+        if(!errors || errors.length  == 0){
+            passVlidation = true
+            await login.login({
+                email: credentials.value.email,
+                password: credentials.value.password
+            })
+        }
+    })
+
 }
 </script>
